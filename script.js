d964501a8bb225999919ae16758521acabb53635
Aman Madd <29maddhesiaaman@gmail.com>
2023-01-31 22:07:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Just a check
+console.log("Loaded successfully");
+
+
+// ---------------------using vanilla javascript
+var colors=["red","yellow",'PINK',"green","purple","Aqua","black","Wheat","Brown","Coral","Crimson","orange","DarkGreen","DarkMagenta","deeppink"];
+var btn=document.getElementById('btn');
+var container=document.getElementById('container');
+
+btn.addEventListener('click',function(){
+    var color=colors[Math.floor(Math.random() * colors.length)];
+    console.log(color);
+    container.innerHTML+=`<div class="ball" style="background-color:${color};"></div>`
+    // container.innerHTML+='<h1>Amajhgjkhsdjivhgih</h1>'
+})
+
+console.log(colors[Math.floor(Math.random() * colors.length)]);
+
+
+
+
+
+
+// Using  JQuery
+// var btn=$('#btn');
+// btn.click(function(){
+//     alert("btn clicked via Jquery");
+// })
+
+
