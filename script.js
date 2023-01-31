67f5ca46d7e31416b3bc848a31fc9e81ba9de0c2
Aman Madd <29maddhesiaaman@gmail.com>
2023-01-31 22:27:04 +0530

    modified the js code into jQUERy

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Just a check
-console.log("Loaded successfully");
+console.log("Loaded successfully ");
 
 
-// ---------------------using vanilla javascript
-var colors=["red","yellow",'PINK',"green","purple","Aqua","black","Wheat","Brown","Coral","Crimson","orange","DarkGreen","DarkMagenta","deeppink"];
-var btn=document.getElementById('btn');
-var container=document.getElementById('container');
+// // ---------------------using vanilla javascript
+// var colors=["red","yellow",'PINK',"green","purple","Aqua","black","Wheat","Brown","Coral","Crimson","orange","DarkGreen","DarkMagenta","deeppink"];
+// var btn=document.getElementById('btn');
+// var container=document.getElementById('container');
 
-btn.addEventListener('click',function(){
-    var color=colors[Math.floor(Math.random() * colors.length)];
-    console.log(color);
-    container.innerHTML+=`<div class="ball" style="background-color:${color};"></div>`
-    // container.innerHTML+='<h1>Amajhgjkhsdjivhgih</h1>'
-})
+// btn.addEventListener('click',function(){
+//     var color=colors[Math.floor(Math.random() * colors.length)];
+//     console.log(color);
+//     container.innerHTML+=`<div class="ball" style="background-color:${color};"></div>`
+//     // container.innerHTML+='<h1>Amajhgjkhsdjivhgih</h1>'
+// })
 
-console.log(colors[Math.floor(Math.random() * colors.length)]);
+// console.log(colors[Math.floor(Math.random() * colors.length)]);
 
 
 
@@ -22,9 +22,14 @@ console.log(colors[Math.floor(Math.random() * colors.length)]);
 
 
 // Using  JQuery
-// var btn=$('#btn');
-// btn.click(function(){
-//     alert("btn clicked via Jquery");
-// })
+var btn=$('#btn');
+var container=$("#container");
+var colors=["red","yellow",'PINK',"green","purple","Aqua","black","Wheat","Brown","Coral","Crimson","orange","DarkGreen","DarkMagenta","deeppink"];
+btn.click(function(){
+        var color=colors[Math.floor(Math.random() * colors.length)];
+        console.log(color);
+        container.append(`<div class="ball" style="background-color:${color};"></div>`);
+        
+})
 
 
